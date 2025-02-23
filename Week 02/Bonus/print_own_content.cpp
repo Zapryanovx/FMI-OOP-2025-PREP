5414ee2df63cd84db7f2d1cf51681d5a9f44ed6e
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2025-02-23 19:31:38 +0200

    Update print_own_content.cpp

--- Week 02/Bonus/print_own_content.cpp
+++ Week 02/Bonus/print_own_content.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//#include <fstream>
-//
-//void printSource(std::ifstream& fileIn)
-//{
-//	char ch = ' ';
-//	while (!fileIn.eof())
-//	{
-//		ch = fileIn.get();
-//		std::cout << ch;
-//	}
-//}
-//
-//int main()
-//{
-//	std::ifstream fileIn("print_own_content.cpp");
-//	if (!fileIn.is_open())
-//	{
-//		return -1;
-//	}
-//
-//	printSource(fileIn);
-//
-//	return 0;
-//}
+#include <iostream>
+#include <fstream>
+
+void printSource(std::ifstream& fileIn)
+{
+	char ch = ' ';
+	while (!fileIn.eof())
+	{
+		ch = fileIn.get();
+		std::cout << ch;
+	}
+}
+
+int main()
+{
+	std::ifstream fileIn("print_own_content.cpp");
+	if (!fileIn.is_open())
+	{
+		return -1;
+	}
+
+	printSource(fileIn);
+
+	return 0;
+}
